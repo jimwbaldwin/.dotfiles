a932780783c2d235323418145f605dabbfb3508c
Jim Baldwin <14864200+jimwbaldwin@users.noreply.github.com>
2026-05-30 07:42:45 +1000

    fix some nvim issues

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -305,7 +305,6 @@ require('lazy').setup({
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
@@ -490,7 +489,7 @@ require('lazy').setup({
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
+      require('nvim-treesitter').setup(opts)
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
